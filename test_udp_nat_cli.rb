33b4a9f05a7e29b75072328cdee3cae129a14a24
Andrew De Ponte <cyphactor@gmail.com>
2013-02-26 00:29:56 -0800

    Added logic to initator and listener client.
    I did this without testing so that we could try and test it together.

--- test_udp_nat_cli.rb
+++ test_udp_nat_cli.rb
@@ -8,7 +8,7 @@ remote_node_id = ARGV[1]
 class MatchMaker
   def initialize
     @s = UDPSocket.new
-    @s.bind('0.0.0.0', 8341)
+    @s.bind('', 8341)
     @host = 'util.devscapades.com'
     @port = 9523
     @payload_size = 16
@@ -34,8 +34,43 @@ class MatchMaker
     @s.send("connect:#{remote_node_id}", 0, @host, @port)
     payload, sender = @s.recvfrom(@payload_size * 10)
 
-    payload_array = payload.split(':')
-    puts payload_array.inspect
+    action, response, rem_ip, rem_port, mode = payload.split(':')
+    if action == 'connect' && response == 'success'
+      puts "Received external ip (#{rem_ip}), port (#{rem_port}), mode (#{mode})"
+      if mode == 'listener'
+        puts "Punching hole in firewall for UDP host (#{rem_ip}) and port (#{6311})"
+        punch = UDPSocket.new
+        punch.bind('', 6311)
+        punch.send('', 0, rem_ip, 6311)
+        punch.close
+        puts "Punched hole."
+
+        puts "Listening for data"
+        # Bind for receiving
+        udp_in = UDPSocket.new
+        udp_in.bind('0.0.0.0', 6311)
+        puts "Binding to local port 6311"
+
+        loop do
+          # Receive data or time out after 5 seconds
+          if IO.select([udp_in], nil, nil, rand(4))
+            data = udp_in.recvfrom(1024)
+            remote_port = data[1][1]
+            remote_addr = data[1][3]
+            puts "Response from #{remote_addr}:#{remote_port} is #{data[0]}"
+          end
+        end
+      else if mode == 'initiator'
+        udp_out = UDPSocket.new
+        udp_out.bind('', 6311)
+        loop do
+          udp_out.send("time:#{Time.now.to_s}", 0, rem_ip, 6311)
+          sleep 2
+        end
+      else
+        puts "ERROR: Uknown connection mode"
+      end
+    end
   end
 end
 
