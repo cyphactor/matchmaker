ab4aeb5b96e0324d3d171cfc939fec4f303fa505
Andrew De Ponte <cyphactor@gmail.com>
2013-03-13 00:12:56 -0700

    Reworked some of the bindings in client.

--- test_udp_nat_cli.rb
+++ test_udp_nat_cli.rb
@@ -49,21 +49,22 @@ class MatchMaker
       puts "Received external ip (#{rem_ip}), port (#{rem_port}), mode (#{mode})"
       if mode == 'listen'
         puts "Punching hole in firewall for UDP host (#{rem_ip}) and port (#{PUNCH_PORT})"
-        punch = UDPSocket.new
+        # punch = UDPSocket.new
         # punch.bind('', PUNCH_PORT)
-        punch.send('now-it-is-something', 0, rem_ip, PUNCH_PORT)
-        punch.close
+        # punch.send('now-it-is-something', 0, rem_ip, PUNCH_PORT)
+        # punch.close
+        @s.send('now-it-is-something', 0, rem_ip, PUNCH_PORT)
         puts "Punched hole."
 
         puts "Listening for data"
         # Bind for receiving
-        udp_in = UDPSocket.new
-        udp_in.bind('0.0.0.0', PUNCH_PORT)
+        # udp_in = UDPSocket.new
+        # udp_in.bind('0.0.0.0', PUNCH_PORT)
         puts "Binding to local port PUNCH_PORT"
 
         loop do
           # Receive data or time out after 5 seconds
-          if IO.select([udp_in], nil, nil, rand(4))
+          if IO.select([@s], nil, nil, rand(4))
             data = udp_in.recvfrom(1024)
             remote_port = data[1][1]
             remote_addr = data[1][3]
@@ -71,10 +72,10 @@ class MatchMaker
           end
         end
       elsif mode == 'initiate'
-        udp_out = UDPSocket.new
-        udp_out.bind('', PUNCH_PORT)
+        # udp_out = UDPSocket.new
+        # udp_out.bind('', PUNCH_PORT)
         loop do
-          udp_out.send("time:#{Time.now.to_s}", 0, rem_ip, rem_port)
+          @s.send("time:#{Time.now.to_s}", 0, rem_ip, rem_port)
           puts "Sent time to #{rem_ip}:#{rem_port}"
           sleep 2
         end
